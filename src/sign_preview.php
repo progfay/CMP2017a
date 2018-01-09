737850ade3b0fe38215134727dc529ffd4640e13
progfay <shun1856shun@gmail.com>
2018-01-10 02:43:12 +0900

    feat(favicon): add favicon to all pages

--- src/sign_preview.php
+++ src/sign_preview.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>サイン確認</title>
+  <link rel="shortcut icon" href="IMG/favicon.ico">
+  <title>Sign Preview</title>
 </head>
 <body>
   <center>
--- src/trade.php
+++ src/trade.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<meta name="format-detection" content="telephone=no">
+	<link rel="shortcut icon" href="IMG/favicon.ico">
 	<title>Trade</title>
 	<link rel="stylesheet" href="dist/bootstrap.min.css">
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
